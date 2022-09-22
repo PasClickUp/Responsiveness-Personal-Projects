3aba9ad46fee91ee275302a968157cff993db1bd
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-09-22 10:32:24 +0100

    PASClickUp

---
+++ Task list Project/app.js
@@ -0,0 +1,14 @@
+// let form = document.querySelector('form')
+// let newTask = document.getElementById('#space')
+
+// form.addEventListener('submit', runEvent)
+
+// function runEvent(e) {
+//     console.log(`EVENT TYPE: ${e.type}`)
+
+//     console.log(newTask.value)
+
+//     e.preventDefault()
+// }
+
+fetch('https://reqres.in/')
